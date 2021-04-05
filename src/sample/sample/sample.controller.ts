93a6e2b18b54c1b8a73424b76037476c5e9b8ee6
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-05 21:37:03 +0530

    crud with dto

--- src/sample/sample/sample.controller.ts
+++ src/sample/sample/sample.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import {SampleService} from "./sample.service";
 import {SampleModel} from "./sample.model";
+import {CreateSampleDto} from "./dto/createSample-dto";
 
 @Controller('sample')
 export class SampleController {
@@ -19,7 +20,22 @@ export class SampleController {
     }
 
     @Post()
-    createSample(@Body('name') name:string){
-        return this.sampleService.createSample(name);
+    createSample(@Body() createSampleDto: CreateSampleDto){
+        return this.sampleService.createSample(createSampleDto);
+    }
+
+    @Get(':id')
+    getSampleById(@Param('id') id:number){
+        return this.sampleService.getSampleById(id);
+    }
+
+    @Delete(':id')
+    deleteSample(@Param('id') id:number){
+        return this.sampleService.deleteSample(id);
+    }
+
+    @Patch(':id')
+    updateSample(@Body('name') name:string, @Param('id') id:number){
+        return this.sampleService.updateSample(id, name);
     }
 }
--- src/sample/sample/sample.service.ts
+++ src/sample/sample/sample.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {SampleModel} from "./sample.model";
 import { v4 as uuidv4 } from 'uuid';
+import {CreateSampleDto} from "./dto/createSample-dto";
 
 @Injectable()
 export class SampleService {
@@ -16,12 +17,28 @@ export class SampleService {
         return this.samples;
     }
 
-    createSample(name: string){
+    getSampleById(id:number) : SampleModel{
+        return this.samples.find((sample) => sample.id === id);
+    }
+
+    createSample(createSampleDto : CreateSampleDto){
+        const { name } = createSampleDto
         const sample: SampleModel = {
             id:uuidv4(),
             name
         }
         this.samples.push(sample);
         return sample;
+    }
+
+    deleteSample(id: number) {
+        this.samples = this.samples.filter((sample)=> sample.id !== id)
+        return this.samples
+    }
+
+    updateSample(id:number, name:string){
+        const sample = this.getSampleById(id);
+        sample.name = name;
+        return name;
     }
 }
